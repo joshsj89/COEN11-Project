082a50a967160f37d080092c6b390eebc7aeecc1
joshsj89 <joshsj89@gmail.com>
2021-12-10 05:16:17 -0800

    Made more efficient and Added comments

--- project.c
+++ project.c
@@ -23,28 +23,25 @@ int main(int argc, char *argv[])
     encrypt_decrypt(argv[1], argv[2], argv[3]);
 }
 
-void encrypt_decrypt(char *key, char *name1, char *name2)
+void encrypt_decrypt(char *key, char *name1, char *name2) //Encrypt/Decrypt function
 {
     FILE *fp1, *fp2;
 
-    fp1 = fopen(name1, "rb");
-    fp2 = fopen(name2, "wb");
+    fp1 = fopen(name1, "rb"); //Reading from first file
+    fp2 = fopen(name2, "wb"); //Writing to second file
 
     if (fp1 == NULL || fp2 == NULL) //The file does not exist and will be created upon save
         return;
 
     int i, ret;
-    char encrypted;
     char buffer[100];
 
     while ((ret = fread(buffer, sizeof(char), 100, fp1)) > 0)
-    {
         for (i = 0; i < ret; ++i)
         {
-            encrypted = buffer[i] ^ atoi(key);
-            fwrite(&encrypted, sizeof(char), 1, fp2);
+            buffer[i] ^= atoi(key); //Each byte (8 bits) of the first file XOR-ed with the 8-bit key (0-255)
+            fwrite(&buffer[i], sizeof(char), 1, fp2); //Writes the newly encrypted/decrypted byte (8 bits) to the second file
         }
-    }
 
     fclose(fp1);
     fclose(fp2);
